202d422170213dd0c49de7826357d21276d0fdde
MR-Stan <michaelrstanley87@gmail.com>
2019-10-26 20:24:26 -0500

    testing google maps API

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -83,64 +83,106 @@ let appObject = {
         }
     },
 
-    googleMapsApi : function() {
-        let settings = {
-            "async": true,
-            "crossDomain": true,
-            "url": "https://google-maps-geocoding.p.rapidapi.com/geocode/json?language=en&latlng=40.714224%2C-73.96145",
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-host": "google-maps-geocoding.p.rapidapi.com",
-                "x-rapidapi-key": "WKovB7gOJC47zEomeAr3l7fQRRamCWAb6Ski8tIo"
-            }};
-        
-        $.ajax(settings).done(function (response) {
-            console.log(response);
-        });
-    },
-        
+    // gmaps key AIzaSyAX-zoE2bM43iSCFsZHCgdzog3iQ31u04k
 
-    // tollGuruApi : function () {
-    //     $.ajax({
-    //         type: "POST",
-    //         url: "https://dev.tollguru.com/v1/calc/gmaps",
-    //         headers: {"x-api-key": "WKovB7gOJC47zEomeAr3l7fQRRamCWAb6Ski8tIo"},
-    //         data: {
-    //             "from": {
-    //                 "address": "Main str, Dallas, TX"
-    //             },
-    //             "to": {
-    //                 "address": "Addison, TX"
-    //             },
-    //             "waypoints": [
-    //                 { "address": "Plano, TX" },
-    //                 { "address": "Allen, TX" }
-    //             ],
-    //             "vehicleType": "2AxlesAuto",
-    //             "fuelPrice": "2.79",
-    //             "fuelPriceCurrency": "USD",
-    //             "fuelEfficiency": {
-    //                 "city": 24,
-    //                 "hwy": 30,
-    //                 "units": "mpg"
-    //             },
-    //             "departure_time": 1551541566,
-    //             "driver": {
-    //                 "wage": 30,
-    //                 "rounding": 15,
-    //                 "valueOfTime": 0
-    //             }
-    //         }
-    //     }).then(function (response) {
-    //         console.log(response);
-    //     }).catch(function (error) {
-    //          console.log(error);
-    //     });
-    // },
+    googleAPI: function () {
+        //Init the geocoder library
+        var geocoder = new google.maps.Geocoder();
+
+        //array to hold the geo address
+        var geoAddress = [];
+
+        //function framework
+        bytutorialMap = {
+            initNavigateMap: function (mapID, panelDirectionID, startLatitude, startLongitude, endLatitude, endLongitude) {
+                var directionsDisplay = new google.maps.DirectionsRenderer;
+                var directionsService = new google.maps.DirectionsService;
+
+                //initialize the map
+                var map = new google.maps.Map(document.getElementById(mapID), {
+                    zoom: 7,
+                    center: { lat: startLatitude, lng: startLongitude }
+                });
+
+                //clear the direction panel
+                $("#" + panelDirectionID).html("");
+                directionsDisplay.setMap(map);
+                directionsDisplay.setPanel(document.getElementById(panelDirectionID));
+
+                //prepare the latitude and longitude data
+                start = startLatitude + ", " + startLongitude;
+                end = endLatitude + ", " + endLongitude;
+                bytutorialMap.calculateAndDisplayRoute(directionsService, directionsDisplay, start, end);
+            },
+
+            //function to get the driving route
+            calculateAndDisplayRoute: function (directionsService, directionsDisplay, start, end) {
+                directionsService.route({
+                    origin: start,
+                    destination: end,
+                    travelMode: 'DRIVING'
+                }, function (response, status) {
+                    if (status === 'OK') {
+                        directionsDisplay.setDirections(response);
+                    } else {
+                        alert('Directions request failed due to ' + status);
+                    }
+                });
+            },
+
+            //get geolocation based on address
+            codeAddress: function (address) {
+                return new Promise(function (resolve, reject) {
+                    geocoder.geocode({ 'address': address }, function (results, status) {
+                        if (status == google.maps.GeocoderStatus.OK) {
+                            resolve(results);
+                        } else {
+                            reject(Error("Geocode for address " + address + " was not successful for the following reason: " + status));
+                        }
+                    });
+                });
+            },
+
+            //function to get geolocation of both addresses.
+            getGeolocationData: function () {
+                if ($("#originInput").val() != "" && $("#destinationInput").val() != "") {
+                    geoAddress = [];
+                    bytutorialMap.codeAddress($("#originInput").val()).then(function (response) {
+                        var geoData = {
+                            latitude: response[0].geometry.location.lat(),
+                            longitude: response[0].geometry.location.lng()
+                        }
+                        geoAddress.push(geoData);
+                    }).then(function () {
+                        return bytutorialMap.codeAddress($("#destinationInput").val()).then(function (response) {
+                            var geoData2 = {
+                                latitude: response[0].geometry.location.lat(),
+                                longitude: response[0].geometry.location.lng()
+                            }
+                            geoAddress.push(geoData2);
+                        });
+
+                    }).then(function () {
+                        bytutorialMap.initNavigateMap("map", "panel-direction", geoAddress[0].latitude, geoAddress[0].longitude, geoAddress[1].latitude, geoAddress[1].longitude);
+                    });
+                } else {
+                    alert("Please enter both addresses");
+                }
+            },
+
+            //clear entries and map display
+            clearEntries: function () {
+                $("#originInput, #destinationInput").val("");
+                $("#map, #panel-direction").html("");
+            }
+        }
+    }
+
 }
 
 appObject.initialize();
-appObject.googleMapsApi();
+appObject.googleAPI();
+
 
 
 
